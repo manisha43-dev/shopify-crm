ce9380ff955b599de83345ae06ce4716b0b8dc6e
manisha43-dev <manishakumar2507@gmail.com>
2026-05-23 09:07:05 +0530

    fix gitignore

--- server/config/redis.js
+++ server/config/redis.js
@@ -4,6 +4,7 @@ const redis = new Redis({
     host: process.env.REDIS_HOST || 'localhost',
     port: process.env.REDIS_PORT || 6379,
     password: process.env.REDIS_PASSWORD || undefined,
+     tls: process.env.NODE_ENV === 'production' ? {} : undefined,
     retryStrategy: (times) => Math.min(times * 50, 2000),
 })
 
