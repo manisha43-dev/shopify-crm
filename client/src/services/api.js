5ea7253779884db0a8bb5ab74a8f9502e1051a9b
manisha43-dev <manishakumar2507@gmail.com>
2026-05-23 09:38:37 +0530

    fix axios

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:5000/api' });
+const api = axios.create({ baseURL:  process.env.REACT_APP_API_URL + '/api' || 'http://localhost:5000/api' });
 
 // Attach JWT token to every request
 api.interceptors.request.use((config) => {
